a925d650b5f89e5490376eef704951631f479671
EULIR <runmingli_edu@163.com>
2018-09-08 23:46:50 +0800

    main

---
+++ src/org/eulir/brainfuck/Main.java
@@ -0,0 +1,32 @@
+package org.eulir.brainfuck;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+	private static String code;
+
+	public static void readFileByLines(String fileName) {
+		File file = new File(fileName);
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String tempString;
+			while ((tempString = reader.readLine()) != null) sb.append(tempString);
+			code = sb.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void run(){
+		readFileByLines("");
+		Parser p = new Parser(code.toCharArray());
+		p.interpret();
+	}
+
+	public static void main(String[] args) {
+		run();
+	}
+}
